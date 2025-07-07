aa8f5aa273588d02cc4138f36de32ca4cc49a952
Anderson Barros Silva <anderson.tec12@gmail.com>
2025-07-07 16:24:19 -0300

    feat: PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    port:4000
+  plugins: [react(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'Ignite-Timer ',
+      short_name: 'IT',
+      description: 'Controle de tarefas',
+      theme_color: '#00875F',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable'
+        }
+      ]
+    }
+  })
+  ],
+  server: {
+    port: 4000
   }
 })
